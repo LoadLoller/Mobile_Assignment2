b005ee0c107bf3940b04750c2d0f1face5b10e61
hanning1 <hanning1@msu.edu>
2020-11-03 12:07:36 +1100

    revised minSDKVersion to enable multidex

--- app/src/main/java/com/example/mobile_w01_07_5/data/CommentData.kt
+++ app/src/main/java/com/example/mobile_w01_07_5/data/CommentData.kt
@@ -2,22 +2,28 @@ package com.example.mobile_w01_07_5.data
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import java.util.*
+import com.google.firebase.database.FirebaseDatabase
 import java.time.LocalDateTime
+
 
 class CommentData {
     @RequiresApi(Build.VERSION_CODES.O)
     fun allComments(): List<CommentItem> {
+//        var database = FirebaseDatabase.getInstance()
+//        var myRef = database.getReference("message")
+//
+//        myRef.setValue("Hello World!")
+
         return listOf<CommentItem>(
-            CommentItem("TS", "sdfas", LocalDateTime.now(), "Yes I think so!"),
-            CommentItem(
-                "VBP",
-                "sdfss",
-                LocalDateTime.now(), "Yes I think so!"
-            ),
-            CommentItem("VNB", "UCB", LocalDateTime.now(), "Yes I think so!"),
-            CommentItem("TS1", "Yosemite", LocalDateTime.now(), "Yes I think so!"),
-            CommentItem("TS2", "Melbourne Square", LocalDateTime.now(), "Yes I think so!")
+                CommentItem("TS", "sdfas", LocalDateTime.now(), "Yes I think so!"),
+                CommentItem(
+                        "VBP",
+                        "sdfss",
+                        LocalDateTime.now(), "Yes I think so!"
+                ),
+                CommentItem("VNB", "UCB", LocalDateTime.now(), "Yes I think so!"),
+                CommentItem("TS1", "Yosemite", LocalDateTime.now(), "Yes I think so!"),
+                CommentItem("TS2", "Melbourne Square", LocalDateTime.now(), "Yes I think so!")
         )
     }
 }
