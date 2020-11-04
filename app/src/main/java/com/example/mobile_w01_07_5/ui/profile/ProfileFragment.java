f92e4ae35d04e87faf0cc4ad51c26029b6b1f4cd
jin <18907382699@163.com>
2020-11-04 14:15:32 +0800

    changed single comment item

--- app/src/main/java/com/example/mobile_w01_07_5/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mobile_w01_07_5/ui/profile/ProfileFragment.java
@@ -1,16 +1,12 @@
 package com.example.mobile_w01_07_5.ui.profile;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,30 +18,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.mobile_w01_07_5.R;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.squareup.picasso.Picasso;
-
-import java.io.InputStream;
-import java.net.URISyntaxException;
 
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
     private StorageReference mStorageRef;
     private ImageView img;
-    private DatabaseReference reference;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
@@ -57,68 +40,30 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-
-//        todo: remove after testing
-//        mStorageRef = FirebaseStorage.getInstance().getReference();
-//        StorageReference childRef=mStorageRef.child("images/cat.jpg");
-//        childRef.getBytes(1024*1024*30).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-//            @Override
-//            public void onSuccess(byte[] bytes) {
-//                Bitmap bitmap= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
-//                img = (ImageView)root.findViewById(R.id.userImg);
-//                img.setImageBitmap(bitmap);
-//            }
-//        });
-
-        /**
-         * Update User Profile with Firebase Information
-         */
-        // Set Up Hookers
-        ImageView userImg = root.findViewById(R.id.userImg);
-
-        TextView userFollower = root.findViewById(R.id.userFollower);
-        TextView userFollowing = root.findViewById(R.id.userFollowing);
-
-        TextView userEmail = root.findViewById(R.id.userEmail);
-        TextView userPhone = root.findViewById(R.id.userPhone);
-        TextView userFb = root.findViewById(R.id.userFb);
-
-        reference = FirebaseDatabase.getInstance().getReference().child("Users").child("user");
-        reference.addValueEventListener(new ValueEventListener() {
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference childRef=mStorageRef.child("images/cat.jpg");
+        childRef.getBytes(1024*1024*30).addOnSuccessListener(new OnSuccessListener<byte[]>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()){
-                    String userImgStr = snapshot.child("image").getValue().toString();
-                    String userFollowerStr = snapshot.child("followers").getValue().toString();
-                    String userFollowingStr = snapshot.child("following").getValue().toString();
-
-                    String userEmailStr = snapshot.child("email").getValue().toString();
-                    String userPhoneStr = snapshot.child("phone").getValue().toString();
-                    String userFbStr = snapshot.child("userID").getValue().toString();
-
-                    Picasso.get().load(userImgStr).into(userImg);
-
-                    userFollower.setText(userFollowerStr);
-                    userFollowing.setText(userFollowingStr);
-
-                    userEmail.setText(userEmailStr);
-                    userPhone.setText(userPhoneStr);
-                    userFb.setText(userFbStr);
-
-                }else{
-                    Log.d("500", "onDataChange: Error Occurs");
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+            public void onSuccess(byte[] bytes) {
+                Bitmap bitmap= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                img = (ImageView)root.findViewById(R.id.userImg);
+                img.setImageBitmap(bitmap);
             }
         });
-
-
         return root;
     }
 
-
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        StorageReference childRef=mStorageRef.child("images/cat.jpg");
+//        childRef.getBytes(1024*1024).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+//            @Override
+//            public void onSuccess(byte[] bytes) {
+//                Bitmap bitmap= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+//                img = img.findViewById(R.id.userImg);
+//                img.setImageBitmap(bitmap);
+//            }
+//        });
+//    }
 }
