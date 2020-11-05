981af6f1b124e63fa041d1de49bc806703bcc5cb
Anqi Zhao <zhaoanqi.zeref@gmail.com>
2020-11-06 09:38:13 +1100

    *swtiched data structure to match firebase in map *fixed nav to details page

--- app/src/main/java/com/example/mobile_w01_07_5/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/mobile_w01_07_5/ui/dashboard/DashboardFragment.java
@@ -2,6 +2,7 @@ package com.example.mobile_w01_07_5.ui.dashboard;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +21,7 @@ import com.example.mobile_w01_07_5.R;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.NavBackStackEntry;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
@@ -69,8 +71,9 @@ public class DashboardFragment extends Fragment implements OnMapReadyCallback{
     private GoogleMap mMap;
     private MapView mMapView;
     private View root;
-    private ArrayList<StampItem> stampList;
+    private ArrayList<StampItem> stampList = new ArrayList<StampItem>();
     private static final int LOCATION_REQUEST = 9158;
+    private final String TAG = "DashboardFragment";
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -89,27 +92,38 @@ public class DashboardFragment extends Fragment implements OnMapReadyCallback{
 
         MapsInitializer.initialize(getActivity().getApplicationContext());
 
-        stampList = new ArrayList<StampItem>();
+//        stampList = new ArrayList<StampItem>();
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Stamps/stamp");
         ValueEventListener postListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 //Structure: HashMap -> HashMap -> userID -> value: String...
 
-                List<Object> stampsList = (List<Object>)snapshot.getValue();
+                HashMap<Object, Object> stampsList = (HashMap<Object, Object>)snapshot.getValue();
+                Object[] keys = stampsList.keySet().toArray();
+                stampList.clear();
+                if (mMap != null){
+                    mMap.clear();
+                }
 
                 for (int i = 0; i < stampsList.size(); i++)
                 {
-                    HashMap<String, Object> stamp = (HashMap<String, Object>) stampsList.get(i);
+                    //the key is acturally the user id
+                    //but it's not filled in the user_id section in the firebase?
+                    Object key = keys[i];
+//                    Log.d(TAG, key.toString());
+                    HashMap<String, Object> stamp = (HashMap<String, Object>) stampsList.get(key);
                     String stampID = stamp.get("stampID").toString();
                     String userID = stamp.get("userID").toString();
                     String name = stamp.get("name").toString();
                     int rate = Integer.parseInt(stamp.get("rate").toString());
                     String description = stamp.get("description").toString();
                     double locationX = Double.parseDouble(stamp.get("locationX").toString());
+//                    Log.d(TAG, "x: " + locationX);
                     double locationY = Double.parseDouble(stamp.get("locationY").toString());
                     String photo = stamp.get("photo").toString();
                     StorageReference photoUrl = mStoRef.child(photo);
+                    int index = i;
                     photoUrl.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                         @Override
                         public void onSuccess(Uri uri) {
@@ -118,22 +132,53 @@ public class DashboardFragment extends Fragment implements OnMapReadyCallback{
                             StampItem item = new StampItem(stampID, userID, name, rate, description,
                                     locationX, locationY, uri, isHighlyRated);
                             stampList.add(item);
+
+                            if (mMap != null)
+                            {
+                                //add stamp to map
+                                LatLng stamp_pos = new LatLng(item.getLocationX(), item.getLocationY());
+                                Marker marker = mMap.addMarker(new MarkerOptions()
+                                        .position(stamp_pos)
+                                        .title(item.getName())
+                                        .snippet(item.getDescription()));
+                                marker.setTag(stampID);
+                            }
                         }
                     }).addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception exception) {
                             // Handle any errors
+                            Log.d(TAG, "firebase fail in dashboard fragment");
                         }
                     });
 
                 }
 
+                if (mMap != null){
+                    //Add marker for melb uni to say hi:)
+                    LatLng uni_postion = new LatLng(-37.7983459, 144.9598797);
+                    mMap.addMarker(new MarkerOptions()
+                            .position(uni_postion)
+                            .title("The_Dev_Team @ UniMelb")
+                            .snippet("Hi from University of Melbourne!"));
+
+                    LocationManager lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+                    Location current_location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                    //Add a marker at the user's current position based on GPS information
+                    if (current_location != null) {
+                        LatLng current_position = new LatLng(current_location.getLatitude(), current_location.getLongitude());
+                        mMap.addMarker(new MarkerOptions().position(current_position).title("You are here").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(current_position));
+                        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+                    }
+                }
 
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                System.out.print("wojuedebuxing" + error);
+                Log.d(TAG, "firebase cancelled in dashboard fragment");
             }
         };
 
@@ -176,11 +221,12 @@ public class DashboardFragment extends Fragment implements OnMapReadyCallback{
         {
             StampItem stamp = stampList.get(i);
             LatLng stamp_pos = new LatLng(stamp.getLocationX(), stamp.getLocationY());
+//            Log.d(TAG, "reading 2 pos: " + stamp_pos.toString());
             Marker marker = mMap.addMarker(new MarkerOptions()
                     .position(stamp_pos)
                     .title(stamp.getName())
                     .snippet(stamp.getDescription()));
-            marker.setTag(i);
+            marker.setTag(stamp.getStampID());
         }
 
         //Add a marker at the user's current position based on GPS information
@@ -195,12 +241,27 @@ public class DashboardFragment extends Fragment implements OnMapReadyCallback{
             @Override
             public void onInfoWindowClick(Marker marker) {
                 //jump to stamp info page
-                int stampIndex = (int) marker.getTag();
-                StampItem stamp = stampList.get(stampIndex);
-                NavDirections action = HomeFragmentDirections.Companion.actionHomeFragmentToProductInfo(stamp.getStampID());
-                Navigation.findNavController(root).navigate(action);
+                if (marker.getTag() != null)
+                {
+                    String stampID = marker.getTag().toString();
+                    NavDirections action = new NavDirections() {
+                        @Override
+                        public int getActionId() {
+                            return R.id.action_navigation_map_to_productInfo;
+                        }
+
+                        @NonNull
+                        @Override
+                        public Bundle getArguments() {
+                            Bundle b =  new Bundle();
+                            b.putString("productCodeArgument", stampID);
+                            return b;
+                        }
+                    };
+                    Navigation.findNavController(getView()).navigate(action);
+                }
             }
         });
     }
-    
+
 }
